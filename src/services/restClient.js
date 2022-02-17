a539773768b637f5117a76e8ab5f1e0212f22331
Matheus da silva <ms25022003@gmail.com>
2022-02-17 17:36:59 -0300

    Creation of a method to populate the database.

--- src/services/restClient.js
+++ src/services/restClient.js
@@ -1,7 +1,18 @@
 import axios from 'axios';
+import products from '../assets/data/products';
+
 
 const respClient = axios.create({
-  baseURL: 'https://ctd-cemmerce-g3.herokuapp.com/'
+  baseURL: 'https://ctd-e-commerce.herokuapp.com/'
 })
 
+async function checkDatabase() {
+  let response = await respClient.get("/products")
+  if (response.data.length === 0) {
+    await respClient.post("/products", products)
+  }
+  window.location.reload()
+}
+
+checkDatabase();
 export default respClient;
