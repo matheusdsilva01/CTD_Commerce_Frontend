797218b8cd6d8ae132db538c77e0151b514f6a1c
Matheus Silva <ms25022003@gmail.com>
2022-05-13 12:11:12 -0300

    att api

--- src/services/restClient.js
+++ src/services/restClient.js
@@ -1,21 +1,7 @@
 import axios from 'axios';
-import products from '../assets/data/products';
-
 
 const respClient = axios.create({
   baseURL: 'https://ctd-e-commerce.herokuapp.com/'
 })
 
-async function checkDatabase() {
-  let response = await respClient.get("/products")
-  if (response.data.length === 0) {
-    respClient.post("/products/list", products).then(() => {
-      window.location.reload();
-    }).then((e)=>{
-      console.log(e)
-    })
-  }
-}
-
-checkDatabase();
 export default respClient;
