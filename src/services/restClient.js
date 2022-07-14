d888d09284c6e3ef89f35dcafada874f9c0b6550
Matheus Silva <85570707+matheusdsilva01@users.noreply.github.com>
2022-07-14 11:18:30 -0300

    Update restClient.js

--- src/services/restClient.js
+++ src/services/restClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const respClient = axios.create({
-  baseURL: `http://${window.location.hostname}:3001/`
+  baseURL: 'https://ctd-e-commerce.herokuapp.com/'
 })
 
 export default respClient;
